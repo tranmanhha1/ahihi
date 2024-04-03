e510c50eaaaeee9e480f39deee5e201d9d758470
king-berry <111548775+king-berry@users.noreply.github.com>
2024-04-03 16:06:54 +0700

    22w
    22w

---
+++ HRM/HRM/Data/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using HRM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRM.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; } = null!;
+    }
+}
---
+++ HRM/HRM/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace HRM.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+    }
+}
